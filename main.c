0730e01f343b068554039fa5a84be15cd818e83e
juho H <juhoth@ovi.com>
2014-01-22 14:55:58 +0200

    usage of mergesort

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#include "lib/merge_sort.c"
+
+void
+printArr(int *, int);
+
+int
+main(int argc, char *argv[])
+{
+    int ary[] = {99, 54, -2, 77, 111};
+    int len = sizeof(ary) / sizeof(ary[0]);
+
+    printf("Merge sort:\n");
+    printArr(ary, len);
+    mergeSort(ary, len);
+    printArr(ary, len);
+
+    return 0;
+}
+
+void
+printArr(int *ary, int len)
+{
+    printf("[");
+    for (int i = 0; i < (len - 1); i++)
+        printf("%d, ", ary[i]);
+    printf("%d]\n", ary[(len -1)]);
+}
+
